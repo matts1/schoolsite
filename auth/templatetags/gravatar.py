45ccdd15be687cddcdb7ec34deeed0e55b731e1b
matts1 <mattstark75@gmail.com>
2013-09-13 12:46:47 +1000

    Made the default image an identicon (geometric image based on hash of email), fixed a bug where users who were not logged in tried to load a gravatar (but of course, we didn't have their email, so errors...)

--- auth/templatetags/gravatar.py
+++ auth/templatetags/gravatar.py
@@ -4,15 +4,12 @@ import urllib, hashlib, httplib
 
 register = Library()
 
-GRAVATAR_DOMAIN = 'gravatar.com'
-GRAVATAR_PATH = '/avatar/'
 SIZE = 64
-DEFAULT = 'http://www.artifacting.com/blog/wp-content/uploads/2010/11/Abe_Lincoln.jpg'
 
 def gravatar(context):
     email = context['user'].email
     gravatar_url = 'http://www.gravatar.com/avatar/' + hashlib.md5(email.lower()).hexdigest() + '?'
-    gravatar_url += urllib.urlencode({'d':DEFAULT, 's':str(SIZE)})
+    gravatar_url += urllib.urlencode({'d':'identicon', 's':str(SIZE)})
     return '<img src="%s">' % gravatar_url
 
 
